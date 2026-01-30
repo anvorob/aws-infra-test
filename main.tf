17cc3253e88c6952bd41a14385a11c62a0bc5063
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2026-01-31 12:02:53 +1300

    Add initial Terraform configuration for AWS

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region  = "us-west-2"
+  profile = "prod-us"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "anvorob-terraform" 
+    key            = "aws-infra/terraform.tfstate" 
+    region         = "us-east-1"
+    
+    encrypt        = true
+  }
+}
+
+module "app_vpc" {
+  source = "git::https://github.com/anvorob/vpc_module_tf.git"
+
+  cidr = "10.0.0.0/23"
+  name = "production"
+  subnets = {
+    int_a = "10.0.0.0/24"
+    int_b = "10.0.1.0/24"
+    }
+}
