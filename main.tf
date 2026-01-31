cebd605c0cf70dff6b92d075ec74fdeb33f22190
Anatolii Vorobiov <anatolii.vorobiov@gmail.com>
2026-01-31 21:22:00 +1300

    Add security group module for EC2 instance

--- main.tf
+++ main.tf
@@ -23,3 +23,28 @@ module "app_vpc" {
     int_b = "10.0.1.0/24"
     }
 }
+
+module "security_groups"
+{
+
+  name = "ECICE"
+  description = "Allows SSH outbound traffic to the private instance"
+  vpc_id = app_vpc.id
+  
+    egress_rules = map(object({
+      source_security_group_id = ""
+      from_port         = 22
+      ip_protocol       = tcp
+      to_port           = 22
+    }))
+}
+
+module "ec2_instance" {
+  source = "git::git@github.com:anvorob/ec2_module_tf.git"
+
+subnet_id = 
+key_name = 
+sg_list = []
+instance_type = "t2.micro"
+name = "Test instance"
+}
